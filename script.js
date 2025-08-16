cb19356c91a048fc3da57c6ee5151bef443f7193
Avr1lls <159795839+SaraLeguizamonM@users.noreply.github.com>
2025-08-15 23:40:31 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Scroll suave en navegación
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
-    });
-  });
-});
